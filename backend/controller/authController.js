c9a1b6b853c9e8cb75f359f28f0776d16439a97b
Suraj Navale <surajnawale4056@gmail.com>
2026-04-21 13:57:08 +0530

    Enhance authentication flow by adding validation checks in signupUser and updating user model constraints

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,26 +1,36 @@
 import User from '../models/User.js';
 import bcrypt from 'bcrypt';
 
-export const signupUser = async(req , res)=>{
-    try {
-        const{ name , email , password} = req.body ;
-        const userExists = await User.findOne(email);
+export const signupUser = async (req, res) => {
+  console.log(req.body);
 
-        if(userExists){
-            res.status(500).json({message:"User alrady Exists " })
-        }
-        const hashPassword = await bcrypt.hash(password , 10);
-
-        User.create({
-            name,
-            email,
-            password : hashPassword,
-        });
-
-        res.json({message:"User register successfully"});
-
-    }catch(error){
-        res.status(500).json({message:"Server error" , error:error.message})
+  try {
+    if (!req.body) {
+      return res.status(400).json({ message: "Body is missing" });
     }
- 
-}
+
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+
+    res.status(201).json({ message: "User registered successfully" });
+
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
+  }
+};
