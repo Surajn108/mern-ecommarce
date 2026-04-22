66eade463b6de1401442713abdbff0ecd169496c
Suraj Navale <surajnawale4056@gmail.com>
2026-04-22 10:43:03 +0530

    Refactor Signup component to include form state management and handle form submission, while adding necessary input fields and styling

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {signupUser} from '../controller/authController.js';
 
+
 const router = express.Router();
 
 router.post('/signup' , signupUser)
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,33 +1,86 @@
 import { useState } from "react";
-import api from '../api/axios.js';
+import api from "../api/axios.js";
+
+export default function Signup() {
+
+    const [form , setForm] = useState({
+        name:"",
+        email:"",
+        password:"", 
+    })
+
+    const [msg , setMsg] = useState("");
+
+    const handleChange = (e)=>{
+        setForm
+    }
+
+    const handleSubmit=(e)=>{
+        e.preventDefault;
+    }
+
+    return( 
+<form
+    onSubmit={handleSubmit}
+    class="mx-auto max-w-md space-y-4 rounded-lg border border-gray-300 bg-gray-100 p-6"
+  >
+    <div>
+      <label class="block text-sm font-medium text-gray-900" for="name">
+        Name
+      </label>
+
+      <input
+        class="mt-1 w-full rounded-lg border-gray-300 focus:border-indigo-500 focus:outline-none"
+        id="name"
+        type="text"
+        placeholder="Your name"
+        value={form.name}
+        onChange={handleChange}
+        required
+      />
+    </div>
+
+    <div>
+      <label class="block text-sm font-medium text-gray-900" for="email">
+        Email
+      </label>
+
+      <input
+        class="mt-1 w-full rounded-lg border-gray-300 focus:border-indigo-500 focus:outline-none"
+        id="email"
+        type="email"
+        placeholder="Your email"
+      />
+    </div>
+
+    <div>
+      <label class="block text-sm font-medium text-gray-900" for="password">
+        Password
+      </label>
+
+      <input
+        class="mt-1 w-full rounded-lg border-gray-300 focus:border-indigo-500 focus:outline-none"
+        id="password"
+        type="password"
+        placeholder="Your password"
+      />
+    </div>
+
+    <button
+      class="block w-full rounded-lg border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition-colors hover:bg-transparent hover:text-indigo-600"
+      type="submit"
+    >
+      Create account
+    </button>
+
+    {msg && (
+        <div className="mb-4 text-center">
+            {msg}
+        </div>
+    )}
+  </form>
+    )
+  
+};
 
 
-const Signup = ()=>{
-    
-
-    <div action="#" class="mx-auto max-w-md space-y-4 rounded-lg border border-gray-300 bg-gray-100 p-6">
-  <div>
-    <label class="block text-sm font-medium text-gray-900" for="name">Name</label>
-
-    <input class="mt-1 w-full rounded-lg border-gray-300 focus:border-indigo-500 focus:outline-none" id="name" type="text" placeholder="Your name"/>
-  </div>
-
-  <div>
-    <label class="block text-sm font-medium text-gray-900" for="email">Email</label>
-
-    <input class="mt-1 w-full rounded-lg border-gray-300 focus:border-indigo-500 focus:outline-none" id="email" type="email" placeholder="Your email"/>
-  </div>
-
-  <div>
-    <label class="block text-sm font-medium text-gray-900" for="password">Password</label>
-
-    <input class="mt-1 w-full rounded-lg border-gray-300 focus:border-indigo-500 focus:outline-none" id="password" type="password" placeholder="Your password"/>
-  </div>
-
-  <button onSubmit={} class="block w-full rounded-lg border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition-colors hover:bg-transparent hover:text-indigo-600" type="submit">
-    Create account 
-  </button>
-</div>
-        };
-
-export default Signup;
