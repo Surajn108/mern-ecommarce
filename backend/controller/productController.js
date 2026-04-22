89706272fe2408d5ce30aa02f820eb057271c119
Suraj Navale <surajnawale4056@gmail.com>
2026-04-22 17:27:47 +0530

    Refactor productController to use ES6 module syntax, improve error handling, and implement create, read, update, and delete (CRUD) operations for products.

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,65 +1,90 @@
 import Products from "../models/Products";
 
-// Create new Products 
-const createProduct = async (req , res) =>{
-    console.log(req.body);
+// Create new Products
+export const createProduct = async (req, res) => {
+  console.log(req.body);
 
-    try{
-        if (!req.body) {
-            return res.status(400).json({ message: "Body is missing" });
-        }
-
-        const product= await product.create(req,body);
-            res.json({message:"Product Created Successfuly"})
-    }catch(err){
-        res.status(500).json({message:"Product creation Fail" , err});
-
+  try {
+    if (!req.body) {
+      return res.status(400).json({ message: "Body is missing" });
     }
 
-
+    const product = await product.create(req, body);
+    res.json({ message: "Product Created Successfuly" });
+  } catch (err) {
+    res.status(500).json({ message: "Product creation Fail", err });
+  }
 };
-// Get all the Products 
-const getProducts = async (req , res) =>{
-    console.log(req.body);
+// Get all the Products
+export const getProducts = async (req, res) => {
+  console.log(req.body);
 
-    try{
-        const products = await Products.find().sort({ createdAt:-1 });
+  try {
+    const products = await Products.find().sort({ createdAt: -1 });
 
-        res.status(200).json({
-            message:"Products ",
-            products,
-        });
-    }catch(err){
-        res.status(500).json({message:"Products not Found", err});
-
-    }
-}
+    res.status(200).json({
+      message: "Products ",
+      products,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Products not Found", err });
+  }
+};
 
 //Update Products
 
+export const updateProduct = async (req, res) => {
+  console.log(req.body);
 
-const updateProduct = async(req , res)=>{
-    console.log(req.body);
-
-    try {
-        if (!req.body) {
-          return res.status(400).json({ message: "Body is missing" });
-        }
-        
-    }catch (error) {
-        res.status(500).json({
-          message: "Server error",
-          error: error.message,
-        });
-      }
-
-    try{
-        const updateP = await Products.findByIdAndUpdate();
-
-
-    }catch(err){
-        res.status(500).json({message:"Server Error", err});
-
+  try {
+    if (!req.body) {
+      return res.status(400).json({ message: "Body is missing" });
     }
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
+  }
 
-}
+  try {
+    const updateP = await Products.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    res.status(200).json({
+      message: "Product Updated Successfuly",
+      updateP,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Server Error", err });
+  }
+};
+
+//Delete a Product
+
+export const deleteProduct = async (req, res) => {
+  console.log(req.body);
+
+  try {
+    if (!req.body) {
+      return res.status(400).json({ message: "Body is missing" });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
+  }
+
+  try {
+    const deleteP = await Products.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({ message: "Product Deleted Successfuly" });
+  } catch (err) {
+    res.status(500).json({ message: "Server Error", err });
+  }
+};
+
+
+ 
---
+++ backend/routes/productsRoutes.js
@@ -0,0 +1,24 @@
+import express from "express";
+import {
+  createProduct,
+  getProducts,
+  updateProduct,
+  deleteProduct,
+} from "../controller/productController";
+
+const router = express.Router();
+
+
+//Create a product 
+router.post('/add' , createProduct);
+
+//Get all Products
+router.get('/' , getProducts);
+
+//Update Product
+router.put('/update/:id', updateProduct);
+
+//Deletet Product 
+router.delete('/delete/:id', deleteProduct);
+
+export default router ;
